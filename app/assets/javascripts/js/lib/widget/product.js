f07436ee40a13d68996e58c207b0c1d68ab3c394
anxsaiyuki <anxsaiyuki@gmail.com>
2014-04-29 15:12:21 +0800

    database

--- app/assets/javascripts/js/lib/widget/product.js
+++ app/assets/javascripts/js/lib/widget/product.js
@@ -1,7 +1,7 @@
 define(['jquery'],function($){
 	return{
 		start:function(){
-			$.get( "../module/product.php", function( data ) {
+			$.get( "../module/product.html", function( data ) {
 			  $( "#product_container" ).html( data );
 			  console.log( "Product Load was Performed." );
 			});
