e4d244ca964516874d2f82dec9246a4b363abb8e
anxsaiyuki <anxsaiyuki@gmail.com>
2014-04-29 18:28:50 +0800

    many data changes

--- app/assets/javascripts/js/lib/widget/product.js
+++ app/assets/javascripts/js/lib/widget/product.js
@@ -1,7 +1,15 @@
 define(['jquery'],function($){
 	return{
 		start:function(){
-			$.get( "../module/product.html", function( data ) {
+			function getUrlVars() {
+				var vars = {};
+				var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+					vars[key] = value;
+				});
+				return vars;
+			}
+
+			$.get( "../module/product." + getUrlVars()["category"] , function( data ) {
 			  $( "#product_container" ).html( data );
 			  console.log( "Product Load was Performed." );
 			});
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140429064454) do
+ActiveRecord::Schema.define(:version => 20140429092858) do
 
   create_table "products", :force => true do |t|
     t.string   "product_name"
     t.integer  "price"
+    t.string   "category"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
