26f1daad359589b333c80667c9f0bc3fa63ebb0d
anxsaiyuki <anxsaiyuki@gmail.com>
2014-05-02 15:19:24 +0800

    top bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
 	user = User.new(user_name: params[:user][:user_name], password: params[:user][:password], email: params[:user][:email])
 	user.save
+	session[:user_name] = params[:user][:user_name]
 
 	
 	redirect_to register_path
