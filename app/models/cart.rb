991f397d08b8eaa7c608e0afc8ad12b078d7cb44
anxsaiyuki <anxsaiyuki@gmail.com>
2014-05-09 23:16:28 +0800

    finish touches

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,15 @@
 class Cart < ActiveRecord::Base
   attr_accessible :price, :product_id, :quantity, :status, :user_id
   belongs_to :product
+  
+  def total_value
+	quantity * price
+  end
+  
+  
+  
+  
+  
+  
+  
 end
