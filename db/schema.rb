314c4826fd6354c50218b637bfa4b8db68b38f73
anxsaiyuki <anxsaiyuki@gmail.com>
2014-05-09 21:36:37 +0800

    fixed bugs:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 201405051555364) do
+ActiveRecord::Schema.define(:version => 20140505155535) do
 
   create_table "carts", :force => true do |t|
     t.integer  "user_id"
