9d2b8a0cff5343cda77ae657f592941169e4f696
anxsaiyuki <anxsaiyuki@gmail.com>
2014-05-23 13:46:16 +0800

    error handeling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  rescue_from NoMethodError, :with => :render_404
+  
+  def render_404
+	respond_to do |format|
+		format.html { render :file => "/errors/not_found", :status => :not_found }
+	end
+  end
+  
+  
 end
--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -38,8 +38,6 @@ class ProductController < ApplicationController
 			@category = Product.select("category, pack_number").uniq.find_all_by_category(params[:id])
 			@product = Product.includes(:image).where(category: params[:id], pack_number: params[:format]).page(params[:page]).per(25)
 		end 
-	else	
-		redirect_to index_path
 	end
 
   end
