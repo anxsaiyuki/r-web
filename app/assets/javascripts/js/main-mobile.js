637f5690bc61c0712794e0e5af7a04126cd6462c
anxsaiyuki <anxsaiyuki@gmail.com>
2014-05-26 11:03:15 +0800

    login

--- app/assets/javascripts/js/main-mobile.js
+++ app/assets/javascripts/js/main-mobile.js
@@ -15,7 +15,6 @@ require(['jquery'], function() {
 		require(['app-ui'], function() {
 			$.ui.autoLaunch = !1, $.ui.animateHeaders = !1, $.ui.useOSThemes = !1, $.ui.splitview = !1, $.feat.nativeTouchScroll=false, require(['item'], function(a){
 				$(document).ready(function(){
-					$.ui.lockPageBounce=!0;
 					$.ui.useInternalRouting = !1;
 					$.ui.setSideMenuWidth('260px');
 					$.ui.launch();
