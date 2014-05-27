3ab30b9b604bf5c0d82e931a4d01f625c7e9d214
anxsaiyuki <anxsaiyuki@gmail.com>
2014-05-27 12:01:28 +0800

    search

--- app/assets/javascripts/js/main-mobile.js
+++ app/assets/javascripts/js/main-mobile.js
@@ -3,8 +3,10 @@ require.config({
     paths: {
 		"jquery": 'jquery',
 		"jqury_ui": 'jquery_ujs',
+		"jq_app": 'js/lib/jq.appframework',
 		"app-ui": 'js/lib/appframework.ui',
 		"app-slide": 'js/lib/af.slidemenu',
+		"app-ui3": 'js/lib/appframework.ui3',
 		item: 'js/lib/widget/item'
     }
 });
@@ -12,27 +14,28 @@ require.config({
 
 require(['jquery'], function() {
 	require(['jqury_ui'], function() {
-		require(['app-ui'], function() {
-			$.ui.autoLaunch = !1, $.ui.animateHeaders = !1, $.ui.useOSThemes = !1, $.ui.splitview = !1, $.feat.nativeTouchScroll=true, require(['item'], function(a){
-				$(document).ready(function(){
-					$.ui.useInternalRouting = !1;
-					$.ui.setSideMenuWidth('260px');
-					$.ui.launch();
-					//a.start();
-					
-					$('.search_header').on('click', function() { 
+		require(['jq_app'], function() {
+			require(['app-ui3'], function() {
+				$.ui.autoLaunch = !1, $.ui.animateHeaders = !1, $.ui.useOSThemes = !1, $.ui.splitview = !1, $.feat.nativeTouchScroll=true, require(['item'], function(a){
+					$(document).ready(function(){
+						$.ui.useInternalRouting = !1;
+						$.ui.setSideMenuWidth('260px');
+						$.ui.launch();
+						//a.start();
 						
-						$('.search_box').removeClass('off');
-						$('.search_box_dummy').removeClass('off');
-						
-						$('.search_box').addClass('on');
-						$('.search_box_dummy').addClass('on');
+						$('.search_header').on('click', function() { 
+							
+							$('.search_box').removeClass('off');
+							$('.search_box_dummy').removeClass('off');
+							
+							$('.search_box').addClass('on');
+							$('.search_box_dummy').addClass('on');
 
 
+						});
 					});
 				});
 			});
 		});
-
 	});
 });
