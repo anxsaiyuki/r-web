e0e746494476ca203cb4ca9b75699265d4db12e2
anxsaiyuki <anxsaiyuki@gmail.com>
2014-05-28 15:26:21 +0800

    search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,7 @@ class SearchController < ApplicationController
 	@user = User.new
 	
 	@product = Product.includes(:image).where("product_name LIKE :product_name OR product_number LIKE :product_number", {:product_name => "%#{params[:key]}%", :product_number => "%#{params[:key]}%"}).page(params[:page]).per(25)
-	p "========"
-	p @product
+
 	if @product.blank? or params[:key].blank?
 		@error_search = 1
 	else
